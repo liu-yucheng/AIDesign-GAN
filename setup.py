55bbb7514c09261e466caeacc470b7c49a564fc6
Yucheng Liu <liu.r.yucheng@gmail.com>
2021-07-28 21:36:31 +0800

    Fixed gitignore

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def _setup_exes_configs():
 def main():
     setuptools.setup(
         name="aidesign-dcgan",
-        version="0.11.1",
+        version="0.11.2",
         description="AI Design DCGAN Application",
         author="AI Design Team",
         packages=setuptools.find_packages(),

7061e0f21eb30e5f1ea16061f05cfab34546efdf
liu-yucheng <liu.r.yucheng@gmail.com>
2021-12-31 20:01:14 +0800

    Updated logging in blackbox testing

--- aidesign_gan/tests/test_as_blackbox.py
+++ aidesign_gan/tests/test_as_blackbox.py
@@ -4,31 +4,52 @@
 # Last updated by: liu-yucheng
 
 import asyncio
+import os
+import pathlib
 import threading
 import unittest
 
+from os import path
+
 _create_subprocess_shell = asyncio.create_subprocess_shell
+_join = path.join
+_makedirs = os.makedirs
+_Path = pathlib.Path
 _PIPE = asyncio.subprocess.PIPE
 _run = asyncio.run
 _TestCase = unittest.TestCase
 _Thread = threading.Thread
 
+# Private attributes.
 
 _timeout = float(30)
 
+_aidesign_gan_tests_path = str(_Path(__file__).parent)
+_aidesign_gan_repo_path = str(_Path(_aidesign_gan_tests_path).parent.parent)
+_aidesign_gan_test_data_path = _join(_aidesign_gan_repo_path, ".aidesign_gan_test_data")
+_logloc = _join(_aidesign_gan_test_data_path, "log.txt")
 
-async def _async_run_cmd(cmd):
+
+async def _async_run_cmd(cmd, instr=""):
     cmd = str(cmd)
+    instr = str(instr)
 
     subproc = await _create_subprocess_shell(cmd=cmd, stdin=_PIPE, stdout=_PIPE, stderr=_PIPE)
-    result = await subproc.wait()
+    inbytes = instr.encode("utf-8", "replace")
+    out, err = await subproc.communicate(inbytes)
+    exit_code = await subproc.wait()
+
+    out = out.decode("utf-8", "replace").rstrip()
+    err = err.decode("utf-8", "replace").rstrip()
+    result = exit_code, out, err
     return result
 
 
-def _run_cmd(cmd):
+def _run_cmd(cmd, instr=""):
     cmd = str(cmd)
+    instr = str(instr)
 
-    result = _run(_async_run_cmd(cmd))
+    result = _run(_async_run_cmd(cmd, instr))
     return result
 
 
@@ -62,23 +83,82 @@ class _FuncThread(_Thread):
         super().join(timeout=timeout)
         return self._result
 
+# End of private attributes.
+# Public attributes.
+
 
 class TestGANCmd(_TestCase):
     """Tests for the "gan" command."""
 
+    def __init__(self, methodName=""):
+        super().__init__(methodName=methodName)
+        self._log = None
+
+    def setUp(self):
+        super().setUp()
+        _makedirs(_aidesign_gan_test_data_path, exist_ok=True)
+        self._log = open(_logloc, "a+")
+
+        start_info = str(
+            "\n"
+            "- Test case {}\n"
+            "\n"
+        ).format(type(self).__name__)
+        self._log.write(start_info)
+
     def test_normal(self):
         """Tests normal use case."""
+        start_info = "-- Test method {}\n".format(self.test_normal.__name__)
+        self._log.write(start_info)
+
         cmd = "gan"
-        thread = _FuncThread(target=_run_cmd, args=[cmd])
+        thread = _FuncThread(target=_run_cmd, args=[cmd, ""])
         thread.start()
-        exit_code = thread.join(_timeout)
+        exit_code, out, err = thread.join(_timeout)
         timed_out = thread.is_alive()
+
+        out_info = str(
+            "--- \"{}\" stdout\n"
+            "{}\n"
+            "--- End of \"{}\" stdout\n"
+        ).format(
+            cmd,
+            out,
+            cmd
+        )
+        self._log.write(out_info)
+
+        err_info = str(
+            "--- \"{}\" stderr\n"
+            "{}\n"
+            "--- End of \"{}\" stderr\n"
+        ).format(
+            cmd,
+            err,
+            cmd
+        )
+        self._log.write(err_info)
 
         fail_msg = "Running \"{}\" results in an timeout".format(cmd)
         self.assertTrue(timed_out is False, fail_msg)
 
         fail_msg = "Running \"{}\" results in an unexpected exit code: {}".format(cmd, exit_code)
         self.assertTrue(exit_code == 0, fail_msg)
+
+        end_info = "-- End of test method {}\n".format(self.test_normal.__name__)
+        self._log.write(end_info)
+
+    def tearDown(self):
+        super().tearDown()
+        end_info = str(
+            "\n"
+            "- End of test case {}\n"
+            "\n"
+        ).format(type(self).__name__)
+        self._log.write(end_info)
+
+        self._log.flush()
+        self._log.close()
 
 
 def main():
@@ -88,3 +168,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+# End of public attributes.

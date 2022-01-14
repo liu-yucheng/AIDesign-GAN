34b44ff7ee8c5659e21644fe8eff4cd781a36549
liu-yucheng <liu.r.yucheng@gmail.com>
2022-01-14 13:23:29 +0800

    Fixed the version name in setup

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ def _make_default_app_data():
 def main():
     _setup(
         name="aidesign-gan",
-        version="0.58.0",
+        version="0.59.1",
         description="AIDesign GAN Modeling Application",
         author="Yucheng Liu (From The AIDesign Team)",
         packages=_find_packages(),

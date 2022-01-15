13ab2a261160875de9218125a46649d769f6474b
liu-yucheng <liu.r.yucheng@gmail.com>
2022-01-16 00:32:12 +0800

    Fixed documentations

--- aidesign_gan/libs/algos/fair_pred_alt_sgd_algo.py
+++ aidesign_gan/libs/algos/fair_pred_alt_sgd_algo.py
@@ -1,9 +1,9 @@
 """Fair predictive alternative SGD algorithm."""
 
-# Copyright (C) 2022 Yucheng Liu. GNU GPL Version 3.
-# GNU GPL Version 3 copy: https://www.gnu.org/licenses/gpl-3.0.txt
-# First added by: liu-yucheng
-# Last updated by: liu-yucheng
+# Copyright 2022 Yucheng Liu. GNU GPL3 license.
+# GNU GPL3 license copy: https://www.gnu.org/licenses/gpl-3.0.txt
+# First added by username: liu-yucheng
+# Last updated by username: liu-yucheng
 
 import numpy
 

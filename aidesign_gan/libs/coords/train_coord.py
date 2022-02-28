fdb026811581406b04d9a1daac110b104fce15c3
liu-yucheng <liu.r.yucheng@gmail.com>
2022-03-01 04:49:29 +0800

    Refactored configs (OOP to functional); Refactored configs-related coords and configs

--- aidesign_gan/libs/coords/train_coord.py
+++ aidesign_gan/libs/coords/train_coord.py
@@ -9,6 +9,7 @@ from os import path as ospath
 from aidesign_gan.libs import algos
 from aidesign_gan.libs import configs
 from aidesign_gan.libs import contexts
+from aidesign_gan.libs import defaults
 from aidesign_gan.libs import results
 from aidesign_gan.libs.coords import coord
 
@@ -60,47 +61,49 @@ class TrainCoord(_Coord):
         if not self._results_ready:
             self._prep_results()
 
-        self._ccfg = _CoordsConfig(self._model_path)
-        self._mcfg = _ModelersConfig(self._model_path)
+        self._cconfig = _CoordsConfig.load_from_path(self._model_path)
+        self._cconfig_loc = _join(self._model_path, defaults.coords_config_name)
+        self._cconfig = _CoordsConfig.verify(self._cconfig)
 
-        self._ccfg.load()
-        self._mcfg.load()
+        self._mconfig = _ModelersConfig.load_from_path(self._model_path)
+        self._mconfig_loc = _join(self._model_path, defaults.modelers_config_name)
+        self._mconfig = _ModelersConfig.verify(self._mconfig)
 
-        self._results.log_config_locs(self._ccfg.location, self._mcfg.location)
+        self._results.log_config_locs(self._cconfig_loc, self._mconfig_loc)
 
         self._context = _TrainContext()
         self._results.context = self._context
 
         training_key = "training"
-        config = self._ccfg[training_key]
+        config = self._cconfig[training_key]
         self._context.setup_rand(config)
         self._results.log_rand()
 
-        config = self._ccfg[training_key]
+        config = self._cconfig[training_key]
         self._context.setup_hw(config)
         self._results.log_hw()
 
-        config = self._ccfg[training_key]
+        config = self._cconfig[training_key]
         self._context.setup_data(self._dataset_path, config)
         self._results.log_data()
 
-        config = self._mcfg
-        self._context.setup_mods(config)
+        config = self._mconfig
+        self._context.setup_mods(self._model_path, config)
         self._results.log_mods()
 
-        config = self._ccfg[training_key]
+        config = self._cconfig[training_key]
         self._context.setup_mode(config)
         self._results.log_mode()
 
         labels_key = "labels"
-        if labels_key in self._ccfg[training_key]:
-            config = self._ccfg[training_key][labels_key]
+        if labels_key in self._cconfig[training_key]:
+            config = self._cconfig[training_key][labels_key]
             self._context.setup_labels(config=config)
         else:  # elif "labels" not in self.coords_config[training_key]:
             self._context.setup_labels()
         self._results.log_labels()
 
-        config = self._ccfg[training_key]
+        config = self._cconfig[training_key]
         self._context.setup_loops(config)
 
         self._context.setup_stats()
@@ -122,7 +125,7 @@ class TrainCoord(_Coord):
         if not self._context_ready:
             self._prep_context()
 
-        algo_name = self._ccfg["training"]["algorithm"]
+        algo_name = self._cconfig["training"]["algorithm"]
         if algo_name == "alt_sgd_algo":
             self._algo = _AltSGDAlgo()
         elif algo_name == "pred_alt_sgd_algo":
